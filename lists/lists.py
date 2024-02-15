d63c04df2d1cfb9f3ead2c50541c911e8f4f10d1
Aziz <s_aziz_s@bk.ru>
2024-02-15 15:00:59 +0600

    lists folder tasks

--- lists/lists.py
+++ lists/lists.py
@@ -1,23 +1,29 @@
 class ListExercise:
     @staticmethod
     def replace(input_list: list[int]) -> list[int]:
-        """
-        Заменить все положительные элементы целочисленного списка на максимальное значение
-        элементов списка.
-
-        :param input_list: Исходный список
-        :return: Список с замененными элементами
-        """
+        local_max = input_list[0]
+        for i in range(1, len(input_list)):
+            if input_list[i] > local_max:
+                local_max = input_list[i]
+        for i in range(len(input_list)):
+            if input_list[i] > 0:
+                input_list[i] = local_max
+        return input_list
         pass
 
     @staticmethod
     def search(input_list: list[int], query: int) -> int:
-        """
-        Реализовать двоичный поиск
-        Функция должна возвращать индекс элемента
-
-        :param input_list: Исходный список
-        :param query: Искомый элемент
-        :return: Номер элемента
-        """
+        def binary_search_recursive(arr: list[int], low: int, high: int, x: int) -> int:
+            if high >= low:
+                mid = (high + low) // 2
+                if mid < len(arr) and arr[mid] == x:
+                    return mid
+                elif mid < len(arr) and arr[mid] > x:
+                    return binary_search_recursive(arr, low, mid - 1, x)
+                else:
+                    return binary_search_recursive(arr, mid + 1, high, x)
+            else:
+                return -1
+        a = binary_search_recursive(input_list, 0, len(input_list), query)
+        return a
         pass
