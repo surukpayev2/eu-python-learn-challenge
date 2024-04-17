2f3217189a1c4c5669f6ab10c98df3716acace03
Aziz <s_aziz_s@bk.ru>
2024-04-17 17:03:55 +0500

    deleted debag prints

--- filter_map/filter_map.py
+++ filter_map/filter_map.py
@@ -4,9 +4,6 @@ from typing import Any, Callable, List, Tuple
 class FilterMapExercise:
     @staticmethod
     def filter_map(func: Callable[[Any], Tuple[bool, Any]], input_array: List[Any]) -> List[Any]:
-
-        go
-
         """
         Реализовать функцию, которая ведет себя как filter и map. К каждому значению из
         списка применяется функция, которая в ответ возвращает кортеж
