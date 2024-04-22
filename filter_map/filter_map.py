061ffd543ab40761b577c3373eae650d83e7e069
Aziz <s_aziz_s@bk.ru>
2024-04-22 16:17:37 +0500

    filter_map_fixed

--- filter_map/filter_map.py
+++ filter_map/filter_map.py
@@ -6,9 +6,9 @@ class FilterMapExercise:
     def filter_map(func: Callable[[Any], Tuple[bool, Any]], input_array: List[Any]) -> List[Any]:
         filtered_array = []
         for element in input_array:
-            a = func(element)
-            if a[0]:
-                filtered_array.append(a[1])
+            flag, value = func(element)
+            if flag:
+                filtered_array.append(value)
         return filtered_array
 
         pass
